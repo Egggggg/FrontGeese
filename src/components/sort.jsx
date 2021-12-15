aab348504875271dee6b0accb8cfcab08e4138ba
Bee Clark <mrhumanman2@gmail.com>
2021-12-15 18:03:22 -0500

    sort is now a class object

--- src/components/sort.jsx
+++ src/components/sort.jsx
@@ -1,19 +1,39 @@
-import { refreshGeese } from "../func.js";
+import React from "react";
 
-export default function Sort() {
-	return (
-		<div id="sort">
-			<label htmlFor="sort">Sort By:</label>
-			<select name="sort" id="sort" onChange={refreshGeese}>
-				<option value="recent" selected="selected">
-					Recent
-				</option>
-				<option value="old">Old</option>
-				<option value="most_likes">Most Likes</option>
-				<option value="least_likes">Least Likes</option>
-				<option value="name_asc">Name (Ascending)</option>
-				<option value="name_desc">Name (Descending)</option>
-			</select>
-		</div>
-	);
+export default class Sort extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { value: "old" };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	getValue() {
+		return this.state.value;
+	}
+
+	render() {
+		return (
+			<div id="sort">
+				<label htmlFor="sort">Sort By:</label>
+				<select
+					name="sort"
+					id="sort"
+					value={this.state.value}
+					onChange={this.refreshGeese}
+				>
+					<option value="recent">Recent</option>
+					<option value="old">Old</option>
+					<option value="most_likes">Most Likes</option>
+					<option value="least_likes">Least Likes</option>
+					<option value="name_asc">Name (Ascending)</option>
+					<option value="name_desc">Name (Descending)</option>
+				</select>
+			</div>
+		);
+	}
 }
