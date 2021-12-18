aaee1b0da8fea4d06a0d10e1aa3d7d72737b7678
Bee Clark <mrhumanman2@gmail.com>
2021-12-18 13:33:54 -0500

    get goose 2 !

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./components/app";
 import Home from "./routes/home";
+import Goose from "./routes/goose";
 
 ReactDOM.render(
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<App />}>
 				<Route index element={<Home />} />
+				<Route path="geese/:slug" element={<Goose />} />
 			</Route>
 		</Routes>
 	</BrowserRouter>,
